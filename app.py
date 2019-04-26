fb167a880a7398dbca1f14c2a97240243a2d1eac
shounakdatta <shounak14datta@gmail.com>
2019-04-25 22:55:56 -0400

    Strings

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-print("Hello World :)")
+test = "Hello World :)"
+test1 = "Hey"
+test2 = "World"
+print(test)
 print("*" * 10)
+x: int = 1
+print(id(x))
+print(len(test))
+print(test[0])
+print(test[0:3])
+print(f"{test1} {test2}")
+print(test.upper())
+print(test.replace("o", "0"))
+print("Hell" in test)
